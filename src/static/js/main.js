35556817484387652860f776e682e2b4dbfe23f4
conreezy <35410060+conreezy@users.noreply.github.com>
2022-07-16 15:22:37 -0700

    menu updated + style + minor concrete page updates

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,12 @@
-document.getElementById("calc_btn").addEventListener("click", function() {
-  alert("Hello World!");
-});
+// // Service Dropdown button
+// document.getElementById("nav_service").addEventListener("click", function(event) {
+//   var service_dropdown = document.getElementById("service_dropdown_content");
+//   service_dropdown.classList.toggle('show_block');
+// });
+
+// // Service Dropdown Mobile button
+// document.getElementById("nav_service_mobile").addEventListener("click", function(event) {
+//   var service_dropdown_mobile = document.getElementById("service_dropdown_content_mobile");
+//   service_dropdown_mobile.classList.toggle('show_block');
+// });
+
