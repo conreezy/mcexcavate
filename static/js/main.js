4eee99c87272f80d2416c3050963b2a38f5daf71
conreezy <35410060+conreezy@users.noreply.github.com>
2022-08-08 10:45:51 -0700

    recude photo size abd change design on concrete and paving pages

--- static/js/main.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("calc_btn").addEventListener("click", function() {
-  alert("Hello World!");
-});
---
+++ static/js/nav.js
@@ -0,0 +1,37 @@
+//  Burger Menu Button
+document.getElementById("mobile_burger").addEventListener("click", function(event) {
+  var top_nav_mobile = document.getElementById("top_nav_mobile");
+  top_nav_mobile.classList.toggle('show_block');
+});
+
+// Hide mobile nav on resize 
+window.addEventListener("resize", function(event){
+  // console.log("window resize");
+  var top_nav_mobile = document.getElementById("top_nav_mobile");
+  // console.log("top nav variable added");
+  var innerwidth = window.innerWidth;
+  // console.log("width:" + innerwidth);
+  var top_nav_display = window.getComputedStyle(top_nav_mobile).display;cd 
+  // console.log("display:" + top_nav_display);
+
+  if (innerwidth > 768 && top_nav_display === "block") {
+    // console.log("if statement true");
+    top_nav_mobile.classList.remove('show_block');
+  };
+});
+
+//  Service dropdown - desktop
+document.getElementById("nav_service").addEventListener("click", function(event) {
+  // console.log("button clicked");
+  var service_dropdown = document.getElementById("service_dropdown");
+  // console.log(service_dropdown);
+  service_dropdown.classList.toggle('show_block');
+});
+
+//  Service dropdown - mobile
+document.getElementById("nav_service_mobile").addEventListener("click", function(event) {
+  // console.log("button clicked");
+  var service_dropdown_mobile = document.getElementById("service_dropdown_mobile");
+  // console.log(service_dropdown_mobile);
+  service_dropdown_mobile.classList.toggle('show_block');
+});
